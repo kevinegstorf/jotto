a49d1c7257a3d24dd2e51444a0b12ceda5dc154e
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-19 14:36:12 +0200

    added when guessed functionality

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -31,4 +31,28 @@ describe("When no words are guessed", () => {
   });
 });
 
-describe("When words are guessed", () => {});
+describe("When words are guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 }
+  ];
+
+  beforeEach(() => {
+    wrapper = wrapperSetup({ guessedWords });
+  });
+
+  it("should render without errors", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  it("should render 'guessed words' section", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
+  it("should render correct number of guessed words", () => {
+    const guessedWordNode = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordNode.length).toBe(guessedWords.length);
+  });
+});
