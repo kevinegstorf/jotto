32c7c47fbb7a96f1dc869cc0c230175077abf1e0
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-19 17:31:27 +0200

    added input component

--- src/containers/Input.test.js
+++ src/containers/Input.test.js
@@ -5,23 +5,49 @@ import Input from "./Input";
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />);
-  console.log(wrapper.debug());
+  const wrapper = shallow(<Input store={store} />).dive();
+  return wrapper;
 };
-
-setup();
 
 describe("render Input Component", () => {
   describe("word has not been guessed", () => {
-    it("should render without errors", () => {});
-    it("should render input box", () => {});
-    it("should render submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    it("should render without errors", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    it("should render input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    it("should render submit button", () => {
+      const submit = findByTestAttr(wrapper, "submit-button");
+      expect(submit.length).toBe(1);
+    });
   });
 
   describe("word has been guessed", () => {
-    it("should not render without errors", () => {});
-    it("should not render input box", () => {});
-    it("should not render submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    it("should not render without errors", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    it("should not render input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
+    });
+    it("should not render submit button", () => {
+      const submit = findByTestAttr(wrapper, "submit-button");
+      expect(submit.length).toBe(0);
+    });
   });
 });
 
