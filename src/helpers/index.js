182fda0dc77bc7ce735b1aca380725fa40b6a92f
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-22 11:27:15 +0200

    removed console logs

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,6 @@
 export function getLetterMatchCount(guessedWord, secretWord) {
-  console.log(secretWord);
-  console.log(guessedWord);
   const secretLetterSet = new Set(secretWord.split(""));
   const guessedLetterSet = new Set(guessedWord.split(""));
-  console.log("--------");
-  console.log(secretLetterSet);
-  console.log(guessedLetterSet);
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
     .length;
 }
