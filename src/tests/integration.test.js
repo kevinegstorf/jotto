f604d43b4ab705e061e94032f88f07d2c7dfc0b9
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-22 11:55:07 +0200

    added test folder with integration test file

---
+++ src/tests/integration.test.js
@@ -0,0 +1,13 @@
+import { storeFactory } from "../../testUtils/testUtils";
+import { guessWord } from "../actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    it("should update state accordingly for unsuccessfull guess ", () => {});
+    it("should update state accordingly for successfull guess", () => {});
+  });
+  describe("some guessed words", () => {
+    it("should update state accordingly for unsuccessfull guess ", () => {});
+    it("should update state accordingly for successfull guess", () => {});
+  });
+});
