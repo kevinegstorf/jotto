e202d6d7192a461e8e9b6c737f68c57179b89c05
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-22 11:54:42 +0200

    moved test utils to diffirent folder

--- src/components/Congrats.test.js
+++ src/components/Congrats.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { findByTestAttr, checkProps } from "../../test/testUtils";
+import { findByTestAttr, checkProps } from "../../testUtils/testUtils";
 import Congrats from "./Congrats";
 
 const defaultProps = { success: false };
